2eb75c04ab646cf4dee72685a3151951bd81a21b
Carel3DS <carel3d@gmail.com>
2022-07-06 08:40:29 +0200

    First commit

--- src/main/java/es/dws/quidditch/repository/GameRepository.java
+++
@@ -1,8 +0,0 @@
-package es.dws.quidditch.repository;
-
-import es.dws.quidditch.model.Game;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface GameRepository extends JpaRepository<Game,Long> {
-
-}
--- src/main/java/es/dws/quidditch/repository/LocaleRepository.java
+++
@@ -1,8 +0,0 @@
-package es.dws.quidditch.repository;
-
-import es.dws.quidditch.model.Locale;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface LocaleRepository extends JpaRepository<Locale,Long> {
-
-}
